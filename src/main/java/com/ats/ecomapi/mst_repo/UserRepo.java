b70bf19dd3e679c597a4841ff53c690143a44290
maddy <maddy@maddy-Inspiron-3541>
2020-09-23 11:47:49 +0530

    //maddy

--- src/main/java/com/ats/ecomapi/mst_repo/UserRepo.java
+++ src/main/java/com/ats/ecomapi/mst_repo/UserRepo.java
@@ -61,48 +61,49 @@ public interface UserRepo extends JpaRepository<User, Integer>{
 	User findByUserEmailAndDelStatus(String email, int del);	
 	User findByUserEmailAndDelStatusAndUserIdNot(String email, int del, int userId);
 
-//	@Query(value="SELECT\n" + 
-//			"    user.user_id,\n" + 
-//			"    user.user_name,\n" + 
-//			"    user.user_mobile_no,\n" + 
-//			"    user.user_address,\n" + 
-//			"    user.user_email,\n" + 
-//			"    user.reg_date,\n" + 
-//			"    user.profile_pic,\n" + 
-//			"    user.password,\n" + 
-//			"    user.company_Id,\n" + 
-//			"    user.department_id,\n" + 
-//			"    user.user_type,\n" + 
-//			"    user.del_status,\n" + 
-//			"    user.is_active,\n" + 
-//			"    user.ex_int1,\n" + 
-//			"    user.ex_int2,\n" + 
-//			"    user.ex_int3,\n" + 
-//			"    user.ex_float1,\n" + 
-//			"    user.ex_float2,\n" + 
-//			"    user.ex_float3,\n" + 
-//			"    user.ex_date1,\n" + 
-//			"    user.ex_date2,\n" + 
-//			"    user.ex_var1,\n" + 
-//			"    user.ex_var2,\n" + 
-//			"    type.user_type_name AS ex_var3,\n" + 
-//			"    depart.department AS ex_var4\n" + 
-//			"    user.updt_dttime,\n" + 
-//			"    user.insert_dttime ,\n" + 
-//			"    user.role_id,\n" + 
-//			"FROM\n" + 
-//			"     m_user user,\n" + 
-//			"     m_user_type type,\n" + 
-//			"     m_department depart\n" + 
-//			"WHERE\n" + 
-//			"	user.company_Id=:compId AND\n" + 
-//			"    user.del_status=0 AND\n" + 
-//			"    type.del_status=0 AND\n" + 
-//			"    depart.del_status=0 AND\n" + 
-//			"    user.user_type=type.user_type_id AND\n" + 
-//			"    user.department_id=depart.department_id\n" + 
-//			"    ORDER BY user.user_id DESC",nativeQuery=true)
-	@Query(value = " SELECT * FROM m_user where company_id=:compId and del_status=1 and is_active=1 ", nativeQuery = true)
+	@Query(value = " SELECT \n" + 
+			"               user.user_id, \n" + 
+			"               user.user_name, \n" + 
+			"               user.user_mobile_no, \n" + 
+			"               user.user_address, \n" + 
+			"               user.user_email, \n" + 
+			"               user.reg_date, \n" + 
+			"               user.birth_date, \n" + 
+			"               user.profile_pic, \n" + 
+			"               user.is_enrolled, \n" + 
+			"               user.otp_text, \n" + 
+			"               user.password, \n" + 
+			"               user.company_Id, \n" + 
+			"               user.dept_id, \n" + 
+			"               user.user_type_id, \n" + 
+			"               user.del_status, \n" + 
+			"               user.is_active, \n" + 
+			"               user.ex_int1, \n" + 
+			"               user.ex_int2, \n" + 
+			"               user.ex_int3, \n" + 
+			"               user.ex_float1, \n" + 
+			"               user.ex_float2, \n" + 
+			"               user.ex_float3, \n" + 
+			"               user.ex_date1, \n" + 
+			"               user.ex_date2, \n" + 
+			"               user.ex_var1, \n" + 
+			"               user.ex_var2, \n" + 
+			"               user.ex_var3, \n" + 
+			"               utype.user_type_name AS ex_var4 ,\n" + 
+			"               user.updt_dttime, \n" + 
+			"               user.insert_dttime, \n" + 
+			"               user.role_id, \n" + 
+			"               user.maker_user_id \n" + 
+			"           FROM \n" + 
+			"          		\n" + 
+			"                m_user user, \n" + 
+			"                m_user_type utype\n" + 
+			"                \n" + 
+			"           WHERE \n" + 
+			"              user.company_Id=:compId AND \n" + 
+			"               user.del_status=1 AND\n" + 
+			"               utype.user_type_id=user.user_type_id\n" + 
+			"               ORDER BY user.user_id DESC", nativeQuery = true)
 	List<User> getAllUserList(@Param("compId") int compId);
 	
 	User findByUserIdAndDelStatusAndCompanyId(int userId, int del, int compId);

1f55df3a2642abaaf59dcf0d8d16543ebc54baca
maddy <maddy@maddy-Inspiron-3541>
2021-03-16 16:44:18 +0530

    //maddy

--- src/main/java/com/ats/ecomapi/controller/CMSApiController.java
+++ src/main/java/com/ats/ecomapi/controller/CMSApiController.java
@@ -20,62 +20,62 @@ public class CMSApiController {
 
 	@Autowired TermsAndCondtnRepo tAndCRepo;
 	
-	@RequestMapping(value = { "/getTermsAndCondtns" }, method = RequestMethod.POST)
-	public @ResponseBody List<TermsAndCondtn> getTermsAndCondtns(@RequestParam int companyId) {
-
-		List<TermsAndCondtn> list = new ArrayList<TermsAndCondtn>();
-		try {
-			list = tAndCRepo.findByCompanyIdAndDelStatusOrderByTermsIdDesc(companyId, 1);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return list;
-
-	}
-	
-	@RequestMapping(value = { "/addTermsAndConditions" }, method = RequestMethod.POST)
-	public @ResponseBody TermsAndCondtn addTermsAndConditions(@RequestBody TermsAndCondtn tnc) {
-
-		TermsAndCondtn newTnc = new TermsAndCondtn();
-		
-		try {
-			newTnc = tAndCRepo.save(tnc);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return newTnc;
-	}
-	
-	@RequestMapping(value = { "/getTermsAndCondtnById" }, method = RequestMethod.POST)
-	public @ResponseBody TermsAndCondtn getTermsAndCondtnById(@RequestParam int termId) {
-
-		TermsAndCondtn list = new TermsAndCondtn();
-		try {
-			list = tAndCRepo.findByTermsId(termId);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return list;
-
-	}
-	
-	
-	@RequestMapping(value = { "/deleteTermsAndCondtnId" }, method = RequestMethod.POST)
-	public @ResponseBody Info deleteUomById(@RequestParam int termsId) {
-
-		Info info = new Info();
-		try {
-			int res = tAndCRepo.deleteTermsAndCondition(termsId);
-			if (res > 0) {
-				info.setError(false);
-				info.setMessage("Terms & Condition Deleted Successfully");
-			} else {
-				info.setError(true);
-				info.setMessage("Failed to Delete Terms & Condition");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return info;
-	}
+//	@RequestMapping(value = { "/getTermsAndCondtns" }, method = RequestMethod.POST)
+//	public @ResponseBody List<TermsAndCondtn> getTermsAndCondtns(@RequestParam int companyId) {
+//
+//		List<TermsAndCondtn> list = new ArrayList<TermsAndCondtn>();
+//		try {
+//			list = tAndCRepo.findByCompanyIdAndDelStatusOrderByTermsIdDesc(companyId, 1);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return list;
+//
+//	}
+//	
+//	@RequestMapping(value = { "/addTermsAndConditions" }, method = RequestMethod.POST)
+//	public @ResponseBody TermsAndCondtn addTermsAndConditions(@RequestBody TermsAndCondtn tnc) {
+//
+//		TermsAndCondtn newTnc = new TermsAndCondtn();
+//		
+//		try {
+//			newTnc = tAndCRepo.save(tnc);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return newTnc;
+//	}
+//	
+//	@RequestMapping(value = { "/getTermsAndCondtnById" }, method = RequestMethod.POST)
+//	public @ResponseBody TermsAndCondtn getTermsAndCondtnById(@RequestParam int termId) {
+//
+//		TermsAndCondtn list = new TermsAndCondtn();
+//		try {
+//			list = tAndCRepo.findByTermsId(termId);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return list;
+//
+//	}
+//	
+//	
+//	@RequestMapping(value = { "/deleteTermsAndCondtnId" }, method = RequestMethod.POST)
+//	public @ResponseBody Info deleteUomById(@RequestParam int termsId) {
+//
+//		Info info = new Info();
+//		try {
+//			int res = tAndCRepo.deleteTermsAndCondition(termsId);
+//			if (res > 0) {
+//				info.setError(false);
+//				info.setMessage("Terms & Condition Deleted Successfully");
+//			} else {
+//				info.setError(true);
+//				info.setMessage("Failed to Delete Terms & Condition");
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return info;
+//	}
 }

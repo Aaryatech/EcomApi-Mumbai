2232fa2eb196a64fab20787bb9c7bd3d471ecc2a
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2021-04-07 18:06:56 +0530

    sac fe data

--- src/main/java/com/ats/ecomapi/fe_repo/GetFlavorTagStatusListRepo.java
+++ src/main/java/com/ats/ecomapi/fe_repo/GetFlavorTagStatusListRepo.java
@@ -13,7 +13,7 @@ public interface GetFlavorTagStatusListRepo extends JpaRepository<GetFlavorTagSt
 	@Query(value = " SELECT m_filter.filter_id,m_filter.filter_name,m_filter.admin_name,m_filter.filter_type_id,m_filter.used_for_filter, " + 
 			" m_filter.cost_affect,m_filter.used_for_description,m_filter.company_id,m_filter.sort_no " + 
 			" FROM m_filter  " + 
-			" WHERE m_filter.filter_type_id IN (4,7,5,12) and m_filter.del_status=1 AND m_filter.is_active=1 AND m_filter.company_id=:companyId " + 
+			" WHERE m_filter.filter_type_id IN (4,7,5,12,1) and m_filter.del_status=1 AND m_filter.is_active=1 AND m_filter.company_id=:companyId " + 
 			" ORDER  BY m_filter.sort_no ,m_filter.filter_type_id ", nativeQuery = true)
 	List<GetFlavorTagStatusList> getFlavorTagStatusListByCompId(@Param("companyId") int companyId);
 	
